957d02492712a4bf729400d4534fb09b0f59d19b
FRJ74 <frankr74@gmail.com>
2024-07-19 14:15:06 -0600

    Add a check for the letter A

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 /* Check a value has been entered */
 const checkBtn = document.getElementById('check-btn');
 const textInput = document.getElementById('text-input');
-const result = document.getElementById('result');
 
 checkBtn.addEventListener('click', function() 
 {
   if(textInput.value === '') {
-    alert('Please input a value');
-  }
+    alert("Please input a value");
+  } else if(textInput.value.length === 1) {
+      result.innerText = `${textInput.value} is a palindrome`;
+  } 
 });
