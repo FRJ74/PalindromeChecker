8382ea7bf67a85376de33f97693e41458afef24f
FRJ74 <frankr74@gmail.com>
2024-07-18 23:03:43 -0600

    Add message to user

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 /* Check a value has been entered */
 const checkBtn = document.getElementById('check-btn');
 const textInput = document.getElementById('text-input');
+const result = document.getElementById('result');
 
 checkBtn.addEventListener('click', function() 
 {
