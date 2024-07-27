674f98f51b03ab83d04cd53c7586301f2bf46fd1
FRJ74 <frankr74@gmail.com>
2024-07-27 14:48:57 -0600

    Add the toLowerCase() & title

--- scripts.js
+++ scripts.js
@@ -5,13 +5,13 @@ const textInput = document.getElementById('text-input');
 checkBtn.addEventListener('click', function() 
 {
 
-    const replaced = textInput.value.replace(/[^A-Za-z0-9]/g, "")
+    const lowerReplaced = textInput.value.toLowerCase().replace(/[^a-z0-9]/g, "")
 
   if(textInput.value === '') {
     alert("Please input a value");
   } else if(textInput.value.length === 1) {
       result.innerText = `${textInput.value} is a palindrome`;
-  } else if(replaced === [...replaced].reverse().join("")) {
+  } else if(lowerReplaced === [...lowerReplaced].reverse().join("")) {
     result.innerText = `${textInput.value} is a palindrome`;
   } 
 });
